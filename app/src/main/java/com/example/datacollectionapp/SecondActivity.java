513fd787533ebbe4b819480689fddf2eb54d71a7
ZapperDude <62452296+ZapperDude@users.noreply.github.com>
2020-04-03 02:47:34 -0500

    Login system

---
+++ app/src/main/java/com/example/datacollectionapp/SecondActivity.java
@@ -0,0 +1,46 @@
+package com.example.datacollectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
